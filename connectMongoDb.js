51b5e37d675f7e11dc9911054641a7d99f93aee8
itsdeepakwith-code <deepakmohanan17@gmail.com>
2024-05-23 21:11:33 +0530

    backend

---
+++ connectMongoDb.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      "mongodb+srv://admin:stackoverflowclone@stackoverflowclone.yfu2eqw.mongodb.net/?retryWrites=true&w=majority&appName=stackoverflowClone" //process.env.CONNECTION_URL
+    );
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
